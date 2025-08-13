3d27dbe57736290ef5afe99a3d6adc703108b605
Koval Sviatoslav <randomercook231@gmail.com>
2025-08-13 12:54:11 +0300

    трохи доробив select, щоб він був трішки гарнішим, але все одно треба буде зробити кастомний select

--- src/components/SelectRanks/SelectRanks.tsx
+++ src/components/SelectRanks/SelectRanks.tsx
@@ -1,13 +1,25 @@
 import { raleway } from '@/assets/fonts/fonts';
 import styles from './SelectRanks.module.scss'
+import Image from 'next/image';
 
 export default function SelectRanks() {
   return (
     // в майбутньому його треба зробити кастомним
-    <select name="selectRanks" className={`${styles.selectRanks} ${raleway.className}`}>
-      <option value="Всі ранги">Всі ранги</option>
-      <option value="lorem">Lorem</option>
-      <option value="lorem ipsum">Lorem ipsum</option>
-    </select>
+    <div className={styles.selectRanksWrapper}>
+      <select
+        name="selectRanks"
+        className={`${styles.selectRanks} ${raleway.className}`}
+      >
+        <option value="Всі ранги">Всі ранги</option>
+      </select>
+      <div className={styles.selectRanksArrow}>
+        <Image 
+          src={'./down-arrow.svg'}
+          alt='down arrow'
+          width={24}
+          height={24}
+        />
+      </div>
+    </div>
   );
 }
