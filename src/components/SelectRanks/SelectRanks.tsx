f3f72c1cfdcc814a5f7ca1f0d89cb0063c8234eb
Koval Sviatoslav <randomercook231@gmail.com>
2025-08-22 17:12:48 +0300

    зробив кастомний селект, і ще зробив, щоб вибраний ранг не був в списку вибору рангів

--- src/components/SelectRanks/SelectRanks.tsx
+++ src/components/SelectRanks/SelectRanks.tsx
@@ -3,9 +3,10 @@
 import { raleway } from "@/assets/fonts/fonts";
 import styles from "./SelectRanks.module.scss";
 import Image from "next/image";
-import { useLocale } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import { Translations } from "@/assets/achievementsList";
 import useSelectRanksStore from "@/lib/states/SelectRanksState";
+import { useState } from "react";
 
 interface RanksList {
   id: number;
@@ -48,35 +49,77 @@ const ranksList: RanksList[] = [
   },
 ];
 
-export default function SelectRanks() {
-  const locale = useLocale();
-  const { setRank } = useSelectRanksStore();
+const RanksListWrapper = ({
+  open,
+  setOpen,
+  locale,
+}: {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  locale: string;
+}) => {
+  const { rank, setRank } = useSelectRanksStore();
+
+  function handleClick(selectedRank: "all" | "rank1" | "rank2" | "rank3") {
+    setOpen(!open);
+    setRank(selectedRank);
+  }
+
+  const filteredRanks = ranksList.filter((el) => el.rankType !== rank);
 
   return (
-    // в майбутньому його треба зробити кастомним
+    <div className={styles.ranksListWrapper}>
+      {filteredRanks.map((el) => (
+        <div
+          className={styles.ranksListElem}
+          key={el.id}
+          onClick={() => handleClick(el.rankType)}
+        >
+          <p>{el.rankText[locale]}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default function SelectRanks() {
+  const locale = useLocale();
+  const t = useTranslations("homePage");
+  const [open, setOpen] = useState<boolean>(false);
+  const { rank } = useSelectRanksStore();
+
+  return (
     <div className={styles.selectRanksWrapper}>
-      <select
-        name="selectRanks"
-        className={`${styles.selectRanks} ${raleway.className}`}
+      <div
+        className={`${styles.selectRanks} ${
+          open === true ? styles.active : ""
+        } ${raleway.className}`}
+        onClick={() => setOpen(!open)}
       >
-        {ranksList.map((el) => (
-          <option
-            value={el.rankText[locale]}
-            key={el.id}
-            onClick={() => setRank(el.rankType)}
-          >
-            {el.rankText[locale]}
-          </option>
-        ))}
-      </select>
-      <div className={styles.selectRanksArrow}>
-        <Image
-          src={"./down-arrow.svg"}
-          alt="down arrow"
-          width={24}
-          height={24}
-        />
+        <h1>
+          {rank === "all"
+            ? t("achievements.selectRanks.selectOption.all")
+            : rank === "rank1"
+            ? t("achievements.selectRanks.selectOption.rank1")
+            : rank === "rank2"
+            ? t("achievements.selectRanks.selectOption.rank2")
+            : rank === "rank3"
+            ? t("achievements.selectRanks.selectOption.rank3")
+            : ""
+          }
+        </h1>
+        <div className={styles.selectRanksArrow}>
+          <Image
+            src={"./down-arrow.svg"}
+            alt="down arrow"
+            width={24}
+            height={24}
+          />
+        </div>
       </div>
+      {open && (
+        <RanksListWrapper open={open} setOpen={setOpen} locale={locale} />
+      )}
     </div>
   );
 }
